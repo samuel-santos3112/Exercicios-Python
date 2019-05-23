2aeb3cb768de3a2aa1b68913ef55bf220e19f68d
samuel <samuelsss2016@gmail.com>
2019-05-23 13:24:45 -0300

    third commit

---
+++ ConversaoCentimetroMetro.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3 /home/samuel/Documentos/Python/ConversaoMetroCentimetro.py
+#coding: utf-8
+
+metros = input("Informe a quantidade exata de metros: ")
+
+convertCentimetros = float (metros) * 100
+
+print("A quantidade de",metros,"em centímetros é: ",convertCentimetros)
