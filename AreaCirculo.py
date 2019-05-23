838c024e9c4a3eed96e0a43692ecdc054cd67634
samuel <samuelsss2016@gmail.com>
2019-05-23 13:19:27 -0300

    first commit

---
+++ AreaCirculo.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3 /home/samuel/Documentos/Python/AreaCirculo.py
+#coding: utf-8
+
+
+
+raio = input("Informe o tamanho do raio do circulo: ")
+
+areaCirculo = 3.1416 * float (raio) ** 2
+
+print("A área desse circulo é: ",areaCirculo)
---
+++ CalculoSalario.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3 /home/samuel/Documentos/Python/CalculoSalario.py
+#coding: utf-8
+
+valorHora = input("Informe valor hora : ")
+hora = input("Informe quantidade de horas trabalhadas: ")
+
+salario = float (valorHora) * int (hora)
+ 
+print ("Salário :",salario)
+
